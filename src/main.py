b607e4d82412000505bf0610061024452d0b0561
Joanna Iwanska <joanna.iwanska@cabiri.io>
2025-05-24 16:49:39 +0100

    feat: add HTMLNode

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from htmlnode import HTMLNode
 from textnode import TextNode, TextType
 
 def main():
     text_node = TextNode("Hello World", TextType.BOLD, "https://www.boot.dev")
     print(text_node)
+    html_node = HTMLNode("a", "Click Me", props = {"class": "primary", "href": "https://boot.dev"})
+    print(html_node)
+    print(html_node.props_to_html())
   
 if __name__ == "__main__":
     main()
