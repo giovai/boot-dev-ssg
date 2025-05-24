8668c104caa0a40e0af8ad6b95459a0c160ba070
Joanna Iwanska <joanna.iwanska@cabiri.io>
2025-05-24 16:03:01 +0100

    chore: add TextNode unit tests

--- src/main.py
+++ src/main.py
@@ -3,5 +3,6 @@ from textnode import TextNode, TextType
 def main():
     text_node = TextNode("Hello World", TextType.BOLD, "https://www.boot.dev")
     print(text_node)
-    
-main()
+  
+if __name__ == "__main__":
+    main()
