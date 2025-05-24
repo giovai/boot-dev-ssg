12daef5aa41600528ca7b7f50b1cfab11aa1f617
Joanna Iwanska <joanna.iwanska@cabiri.io>
2025-05-24 17:52:32 +0100

    feat: add LeafNode

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 from htmlnode import HTMLNode
+from leafnode import LeafNode
 from textnode import TextNode, TextType
 
 def main():
     text_node = TextNode("Hello World", TextType.BOLD, "https://www.boot.dev")
     print(text_node)
+    
     html_node = HTMLNode("a", "Click Me", props = {"class": "primary", "href": "https://boot.dev"})
     print(html_node)
     print(html_node.props_to_html())
+    
+    leaf_node = LeafNode("a", "Click Me", props = {"class": "primary", "href": "https://boot.dev"})
+    print(leaf_node)
+    print(leaf_node.to_html())
   
 if __name__ == "__main__":
     main()
